45cf44fbc5dd8a3d16d89798849f702c66e76a24
mauvaisconseill-oss <mauvaisconseill@gmail.com>
2026-08-28 03:58:44 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -325,7 +325,7 @@ function getOrCreateDateStatusEl(){
     el.id = 'date-status-msg';
     el.className = 'slot-note';
     el.style.marginTop = '8px';
-    el.style.cssText += 'font-size:11px !important; margin-top:8px !important;';
+    el.style.cssText += 'font-size:9.5px !important; margin-top:8px !important;';
     document.getElementById('date_rdv').insertAdjacentElement('afterend', el);
   }
   return el;
