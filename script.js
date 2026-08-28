e100c3aa7e1857437713caa92498a5050526e9f5
mauvaisconseill-oss <mauvaisconseill@gmail.com>
2026-08-28 04:05:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -324,8 +324,6 @@ function getOrCreateDateStatusEl(){
     el = document.createElement('p');
     el.id = 'date-status-msg';
     el.className = 'slot-note';
-    el.style.marginTop = '-8px';
-    el.style.fontSize = '11px';
     document.getElementById('date_rdv').insertAdjacentElement('afterend', el);
   }
   return el;
